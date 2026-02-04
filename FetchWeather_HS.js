887a043fd10a8e828c55b32b8bc61989c37a1f47
jajl123 <57410718+jajl123@users.noreply.github.com>
2026-02-04 08:45:06 -0500

    Update FetchWeather_HS.js

--- FetchWeather_HS.js
+++ FetchWeather_HS.js
@@ -1,7 +1,7 @@
 async function fetchWeather() {
   const params = new URLSearchParams({
-    latitude: 39.922177,
-    longitude: -87.430714,
+    latitude: 36.739824,
+    longitude: -79.444818,
     daily: "temperature_2m_max,temperature_2m_min,precipitation_probability_max,precipitation_hours,weather_code,cloud_cover_mean",
     current: "temperature_2m,precipitation,weather_code,cloud_cover",
     hourly: "temperature_2m,precipitation_probability,cloud_cover,wind_speed_10m,wind_direction_10m,rain,showers,snowfall,weather_code",
